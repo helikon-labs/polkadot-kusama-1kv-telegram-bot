d1732040dec9adce381c7d13dc22ce3064734759
kukabi <kutsalbilgin@gmail.com>
2021-12-02 17:41:40 +0300

    Bump version.

--- modules/config.js
+++ modules/config.js
@@ -9,7 +9,7 @@ const polkadotEraLengthMins = 1440;
 const polkadotSessionLengthMins = 240;
 
 const config = {
-    version: '1.4.14',
+    version: '1.4.15',
     sendReleaseNotes: false,
     mongoDBConnectionURL: process.env.MONGODB_CONNECTION_URL,
     dbName: process.env.DB_NAME,
